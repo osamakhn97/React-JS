781305a987547d2f46d88e38811db4e2c817e551
osama <osamakhn97@gmail.com>
2022-01-12 12:38:40 +0500

    CSS styling in react js

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,60 +1,57 @@
-import React from 'react';
-import { useState } from 'react';
-import GoalForm from './components/GoalForm.js';
-import GetGoals from './components/GetGoals.js';
+import React, { useState } from 'react';
 
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
 
-const App = ()=>{
-  
-const myGoals = [
-  {id:'g1',desc:'Hello'},{id:'g2',desc:'Ali'}
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
 
-];
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
 
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
 
-  const [goals,setGoals] = useState(myGoals);
-  const remove = (item)=>{
-    const newGoals = goals.filter(e=> !(e.id === item));
-    // console.log(item);
-    // console.log(newGoals);
-    setGoals(newGoals);
-  }
-  const get = (goal)=>{
-    // console.log(goal)
-    setGoals((prevGoal)=>{
-      return [goal, ...prevGoal]
-    })
-  }
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
 
-  // let foundGoals = <h2 style = {{'textAlign': 'center', 'color':'green'}}>Goals Found</h2>;
-  let foundGoals = <div>
-    {goals.map(g=>{
-    return <GetGoals 
-    key={g.id}
-    id = {g.id}
-    desc = {g.desc}
-    rem = {remove}
-    
-    />})}
-
-
-  </div>;
-
-  if(goals.length === 0){
-    foundGoals = <h2 style = {{'textAlign': 'center', 'color':'red'}}>No goals Found</h2>
-    
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
   }
 
   return (
     <div>
-      <GoalForm getGoals = {get}/>
-      {foundGoals}
-
-
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
     </div>
-  )
+  );
+};
 
-
-
-}
-export default App
+export default App;
